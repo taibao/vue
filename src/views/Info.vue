a3eed02ae4ec957f85cde6a3ff095f9b3e579885
vitas <vitas@vitasdeMacBook-Pro.local>
2019-01-24 22:08:40 +0800

    提交vue

---
+++ src/views/Info.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    hello info component
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+
+<script>
+  import store from '@/store'
+  export default {
+    name: 'Info',
+    store,
+    data(){
+      return {
+        msg:'this is not problem'
+      }
+    },
+    methods:{
+      add(){
+        debugger;
+        console.log('add Event from info!');
+        store.commit('increase')
+      },
+      output(){
+        console.log('');
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
